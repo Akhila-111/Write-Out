192c3687e1c62aea138ef2533148cb65cb327b27
akhila <ampiliakhila@gmail.com>
2022-03-21 22:18:11 +0530

    second  commit

--- Write_Out/app/src/main/java/com/example/write_out/MainActivity.java
+++ Write_Out/app/src/main/java/com/example/write_out/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.write_out;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout tablayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tablayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+
+
+
     }
 }
