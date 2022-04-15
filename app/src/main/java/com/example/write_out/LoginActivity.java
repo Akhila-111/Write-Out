859bcd43a7b9b202f4646f8f6de8813221746d0d
Akhila-111 <ampiliakhila@gmail.com>
2022-04-15 12:26:46 +0530

    resolved small bugs

--- app/src/main/java/com/example/write_out/LoginActivity.java
+++ app/src/main/java/com/example/write_out/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
 
     FirebaseAuth mAuth ;
-    FirebaseUser user ;
+
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -116,56 +116,6 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
